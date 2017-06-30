a7802dddd04a9e97e4de92b6c0562a99c9d2220f
Pooja Khullar <poojakhullar24@gmail.com>
2017-06-29 17:46:49 -0700

    changes made to build.bat for GoCD

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-echo Let us pretend that this is building something by PK ... and takes 20 seconds
+echo Let us pretend that this is building something ... and takes 20 seconds (changes made by Pooja Khullar)
 
 FOR /l %%A in (1, 1, 20) DO (
   ECHO Building [[01;31m %%A of 20 [00m]
